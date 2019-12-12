5d52498fd1967fd30957a38455e3245fe1013894
kaiz16 <realkaiz7@gmail.com>
2019-12-12 23:18:35 +0800

    Ex 26: Remove uneeded stuff

--- exercise-26-unique-char.rb
+++ exercise-26-unique-char.rb
@@ -3,10 +3,6 @@ print "Enter a sentence : "
 str = gets.chomp
 puts "You have inputted this string : #{str}"
 
-def is_upcase?(char)
-    return char == char.upcase
-end
-
 def unique_characters(str)
 split_chars = str.downcase.split("")
 result = {} # storing characters and their appearances in hash table
@@ -22,31 +18,3 @@ end
 
 puts "The unique characters are : " 
 unique_characters(str).each {|char, value| puts "#{char} - #{value}"}
-
-
-# for i in 0..new_array.length - 1
-#     if !result.include?(new_array[i].upcase) && !result.include?(new_array[i].downcase)
-#         result[str[i]] = new_array.count(new_array[i])
-#     end
-# end
-
-#p result
-# for i in 0..new_array.length - 1
-#     char = new_array[i]
-#     count = 0
-#     new_array.each do |x|
-#         if x == char
-#             count += 1
-#         end
-#     end
-#     if char == " "
-#         result["(space)"] = count
-#     end
-#     result[str[i]] = count
-# end
-
-#p result
-# sorted_hash = result.sort_by {|char, value| value}.reverse # descending order, remove .reverse for ascending
-# sorted_hash.each do |char, value|
-#     puts "#{char} - #{value}"
-# end
