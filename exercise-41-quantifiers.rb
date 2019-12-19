e9878efff73cb924a4b5b813a8861e9c83237da6
kaiz16 <realkaiz7@gmail.com>
2019-12-20 00:45:42 +0800

    Exercise 41 - Quantifiers

---
+++ exercise-41-quantifiers.rb
@@ -0,0 +1,10 @@
+a. /\b(b)(\w*)(e)/
+b. /.*\?$/
+c. /.{1,}\?$/
+d. /^(https?:\/\/)(\S*)$/
+e. /\b[A-Za-z]*i[A-Za-z]*i[A-Za-z]*i[A-Za-z]*\b/
+f. /\S*$/
+g. /^(,\d+){3,6},$/
+h. /^(\d+,){2,5}(\d+)$/
+i. /(^\d+(,\d+){2}$)|(^\d+(,\d+){5,}$)/
+j. /<h([1-6])>.*?<\/h\1>/ # matching a closing tag number with opening tag number by using backreference.
