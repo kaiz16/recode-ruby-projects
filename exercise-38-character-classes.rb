07c592f6ddfb1b0fbb88962d7d9ca646eb274f3d
kaiz16 <realkaiz7@gmail.com>
2019-12-19 23:44:13 +0800

    Exercise 38 - Character Classes

---
+++ exercise-38-character-classes.rb
@@ -0,0 +1,8 @@
+a. /K|k|s/
+b. /cat|cot|cut|bat|bot|but/i
+c. /([0-9A-Ja-j])/
+d. /([^xX\s0-9.])/
+e. /(\d|\W)/
+f. "No. String 'Abc' would match with 2nd expression but not with first."
+g. "Yes. They are same because 1st expression has case insensitive option."
+h. /(\[\^\w-\w\])/
