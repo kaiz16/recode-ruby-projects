f4f4c58171d8108a3fc008517abbdfd58646e209
kaiz16 <realkaiz7@gmail.com>
2020-01-03 21:13:57 +0800

    Updated exercise 51 and 52

--- exercise_51.rb
+++ exercise_51.rb
@@ -1,34 +1,54 @@
-# In Class Exercise: Binary Search
-# Write a method, binary search(array, target), that accepts an array and another input as target
-# The method should use the binary search algorithm to return the position of the element where the target is found
+# You will write a method, binary search(array, target), that accepts an array and another input as target
+# The method should use the iterative binary search algorithm to return the position of the element where the target is found
+# If no target is found, -1 should be returned
+# The recursive solution is not accepted for this exercise
 
-def binary_search(target, array)
-# 1) Make a low and high variable set it to starting index and ending index of the array
-# 2) Make a loop that runs while 'high' is greater than 'low'.
-# 3) Make a mid variable and set it to middle index of the array
-# 4) If array[middle] == target, then it's done
-# 5) Else target < array[middle], then look to the left set high to current middle - 1. Low stays unchanged
-# 6) Else target > array[middle], then look to the right set low to current middle + 1. High stays unchanged
-# 7) Repeat from step 2 until low becomes greater than high.
-# 8) Return -1 if value is not in the array.
-	low = 0
-    high = array.length - 1
-    until low > high 
-        mid = (low + high) / 2
-        case target <=> array[mid]
-        when 0 # Equal to
-            return mid # Step 4
-        when -1 # Less than
-            high = mid - 1  # Step 5
-        when 1 # Greater than
-            low = mid + 1 # Step 6
-        end
-    end 
-    return -1 # Step 8
-end
-
-test_array = (100..200).to_a
-puts binary_search(135, test_array) == 35
-
-test_array = [13, 19, 24, 29, 32, 37, 43]
-puts binary_search(35, test_array) == -1
+# Write Your method code here
+def binary_search(array, target)
+    # 1) Make a low and high variable set it to starting index and ending index of the array
+    # 2) Make a loop that runs while 'high' is greater than 'low'.
+    # 3) Make a mid variable and set it to middle index of the array
+    # 4) If array[middle] == target, then it's done
+    # 5) Else target < array[middle], then look to the left set high to current middle - 1. Low stays unchanged
+    # 6) Else target > array[middle], then look to the right set low to current middle + 1. High stays unchanged
+    # 7) Repeat from step 2 until low becomes greater than high.
+    # 8) Return -1 if value is not in the array.
+        low = 0
+        high = array.length - 1
+        until low > high 
+            mid = (low + high) / 2
+            case target <=> array[mid]
+            when 0 # Equal to
+                return mid # Step 4
+            when -1 # Less than
+                high = mid - 1  # Step 5
+            when 1 # Greater than
+                low = mid + 1 # Step 6
+            end
+        end 
+        return -1 # Step 8
+    end
+    
+    # Driver Code: Do not edit under this line
+    
+    # check_solution runs a single test case and prints whether it was
+    # successful or not.
+    def check_solution(test_number, array, target, expected)
+      actual = binary_search(array, target)
+      if actual != expected
+        puts "Test ##{test_number}: Incorrect value: got #{actual}, expected #{expected}"
+        return false
+      end
+    
+      puts "Test ##{test_number}: Correct"
+      return true
+    end
+    
+    # run_tests runs each of the test cases.
+    def run_tests
+      check_solution(1, (100..200).to_a, 135, 35)
+      check_solution(2, [13, 19, 24, 29, 32, 37, 43], 35, -1)
+    end
+    
+    # Execute the tests.
+    run_tests
--- exercise_52.rb
+++ exercise_52.rb
@@ -1,25 +1,47 @@
 # Write a method, linear_search(array, target), that accepts an array and another input as target
-# The method should use the binary search algorithm to return the position of the element where the target is found
+# The method should use the iterative linear search algorithm to return the position of the element where the target is found
+# If no target is found, -1 should be returned
 # You may not use any Array or Enum methods except length or size
-# You may not use any Enum derived iterations, only for, while or until
+# You may not use any Enum derived iterations. You may use only for, while or until loops
+# The recursive solution is not accepted for this exercise
 
-def linear_search(target, searchfrom)
-# 1) Set the starting index as 0
-# 2) Make a loop that runs array.length - 1 times
-# 3) On each interation, check for the value.
-# 4) If target is found, return index position of the target
-# 5) Else, increment the index by 1
-# 6) Repeat from step 3
-# 7) Return -1 when target is not found.
-	index = 0
-    while index <= searchfrom.length - 1
-        return "Found #{target} at index #{index}" if searchfrom[index] == target
-        index += 1
+# Write Your method code here
+def linear_search(target, array)
+    # 1) Set the starting index as 0
+    # 2) Make a loop that runs array.length - 1 times
+    # 3) On each interation, check for the value.
+    # 4) If target is found, return index position of the target
+    # 5) Else, increment the index by 1
+    # 6) Repeat from step 3
+    # 7) Return -1 when target is not found.
+        index = 0
+        while index <= array.length - 1
+            return index if array[index] == target
+            index += 1
+        end
+        return -1
     end
-    return false
-end
-
-random_numbers = [6,29,18,2,72,19,18,10,37]
-p linear_search(18, random_numbers)
-p linear_search(9, random_numbers)
-
+    
+    # Driver Code: Do not edit under this line
+    
+    # check_solution runs a single test case and prints whether it was
+    # successful or not.
+    def check_solution(test_number, array, target, expected)
+      actual = linear_search(array, target)
+      if actual != expected
+        puts "Test ##{test_number}: Incorrect value: got #{actual}, expected #{expected}"
+        return false
+      end
+    
+      puts "Test ##{test_number}: Correct"
+      return true
+    end
+    
+    # run_tests runs each of the test cases.
+    def run_tests
+      check_solution(1, 18, [6,29,18,2,72,19,18,10,37], 2)
+      check_solution(2, 9, [6, 29, 18, 2, 72, 19, 18, 10, 37], -1)
+    end
+    
+    # Execute the tests.
+    run_tests
